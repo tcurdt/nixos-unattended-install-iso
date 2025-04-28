b1135e9592be9a158558e1c06c047b4d762b6c68
Torsten Curdt <tcurdt@vafer.org>
2025-04-28 22:07:04 +0200

    cleanup

--- configuration.nix
+++ configuration.nix
@@ -33,19 +33,15 @@
   networking.useDHCP = lib.mkDefault true;
   networking.hostName = "nixos";
 
-  # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
-  # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
+  time.timeZone = "UTC";
+  i18n.defaultLocale = "en_US.UTF-8";
 
-  # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
   # console =  {
   #   earlySetup = true;
   #   font = "ter-v16n";
@@ -53,7 +49,6 @@
   #   useXkbConfig = true; # use xkb.options in tty.
   # };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   # users.users.alice = {
   #   isNormalUser = true;
   #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
@@ -63,21 +58,16 @@
   #   ];
   # };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     nano
     gitMinimal
     curl
   ];
 
-  # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # Automatically log in at the virtual consoles.
   services.getty.autologinUser = "root";
 
-  # Allow the user to log in as root without a password.
   users.users.root.initialHashedPassword = "";
 
   system.stateVersion = "24.11";
